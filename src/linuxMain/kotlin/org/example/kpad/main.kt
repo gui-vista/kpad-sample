c98dbf053140ab376cd1f67428bda9c20154a481
Nick Apperley <napperley@gmail.com>
2018-11-17 19:11:14 +1300

    Added functionality to open file in text editor.

--- src/linuxMain/kotlin/org/example/kpad/main.kt
+++ src/linuxMain/kotlin/org/example/kpad/main.kt
@@ -4,8 +4,9 @@ package org.example.kpad
 
 import gtk3.*
 import kotlinx.cinterop.*
+import platform.posix.*
 
-private fun createOpenFileDialog(parent: CPointer<GtkWindow>) {
+internal fun createOpenFileDialog(parent: CPointer<GtkWindow>) {
     val gtkStockCancel = "gtk-cancel"
     val gtkStockOpen = "gtk-open"
     val dialogArgs = arrayOf(GTK_RESPONSE_CANCEL, gtkStockOpen, GTK_RESPONSE_ACCEPT, null)
@@ -17,38 +18,20 @@ private fun createOpenFileDialog(parent: CPointer<GtkWindow>) {
         variadicArguments = *dialogArgs
     )
     val resp = gtk_dialog_run(dialog?.reinterpret())
-    g_print("Response: %d\n", resp)
     if (resp == GTK_RESPONSE_ACCEPT) {
-        // TODO: Open file and display its contents in the editor.
-        g_print("File: %s\n\n", gtk_file_chooser_get_filename(dialog?.reinterpret())?.toKString())
+        val filePath = gtk_file_chooser_get_filename(dialog?.reinterpret())?.toKString()!!
+        updateStatusBar("Opening $filePath...")
+        updateEditor(filePath)
+        updateMainWindowTitle("KPad - ${fileName(filePath)}")
+        updateStatusBar("Ready")
     }
     gtk_widget_destroy(dialog)
 }
 
-private fun openBtnClicked(@Suppress("UNUSED_PARAMETER") widget: CPointer<GtkWidget>, userData: gpointer) {
-    createOpenFileDialog(userData.reinterpret())
-}
-
-private fun activate(app: CPointer<GtkApplication>, @Suppress("UNUSED_PARAMETER") userData: gpointer) {
-    val win = gtk_application_window_new(app)!!
-    val mainLayout = createMainLayout(win.reinterpret())
-    gtk_window_set_title(win.reinterpret(), "KPad")
-    gtk_window_set_default_size(window = win.reinterpret(), width = 600, height = 400)
-    gtk_container_add(win.reinterpret(), mainLayout.reinterpret())
-    gtk_widget_show_all(win)
-}
-
-private fun createMainLayout(win: CPointer<GtkWindow>): CPointer<GtkContainer> {
-    val mainLayout = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)!!
-    val toolbar = createToolbar(win)
-    val editor = createEditor()
-    val statusBar = createStatusBar()
-
-    gtk_container_add(mainLayout.reinterpret(), toolbar.reinterpret())
-    addWidgetToBoxFromTop(box = mainLayout.reinterpret(), widget = editor.reinterpret(), expand = true)
-    addWidgetToBoxFromBottom(box = mainLayout.reinterpret(), widget = statusBar.reinterpret())
-    gtk_widget_grab_focus(editor.reinterpret())
-    return mainLayout.reinterpret()
+internal fun fileName(filePath: String): String = if ('/' in filePath && filePath.length >= 2) {
+    filePath.slice((filePath.lastIndexOf("/") + 1)..(filePath.length - 1))
+} else {
+    ""
 }
 
 internal fun addWidgetToBoxFromTop(
@@ -79,48 +62,10 @@ internal fun addWidgetToBoxFromBottom(
     padding = padding
 )
 
-private fun createEditor(): CPointer<GtkTextView> {
-    val margins = mapOf("left" to 10, "right" to 10, "top" to 10)
-    val editor = gtk_text_view_new()!!
-    gtk_widget_set_margin_left(editor, margins["left"]!!)
-    gtk_widget_set_margin_right(editor, margins["right"]!!)
-    gtk_widget_set_margin_top(editor, margins["top"]!!)
-    gtk_text_view_set_cursor_visible(editor.reinterpret(), TRUE)
-    return editor.reinterpret()
-}
-
-private fun createStatusBar(): CPointer<GtkStatusbar> {
-    val statusBar = gtk_statusbar_new()!!
-    gtk_statusbar_push(statusbar = statusBar.reinterpret(), text = "Ready", context_id = 0)
-    return statusBar.reinterpret()
-}
-
-private fun createToolbar(win: CPointer<GtkWindow>): CPointer<GtkToolbar> {
-    val toolbar = gtk_toolbar_new()!!
-    val newBtn = gtk_button_new_with_label("New")
-    val openBtn = gtk_button_new_with_label("Open")
-    val saveBtn = gtk_button_new_with_label("Save")
-    val newItem = gtk_tool_item_new()
-    val openItem = gtk_tool_item_new()
-    val saveItem = gtk_tool_item_new()
-    val toolItems = arrayOf(newItem, openItem, saveItem)
-
-    if (openBtn != null) {
-        connectGtkSignal(obj = openBtn, actionName = "clicked", data = win, action = staticCFunction(::openBtnClicked))
-    }
-    gtk_container_add(newItem?.reinterpret(), newBtn?.reinterpret())
-    gtk_container_add(openItem?.reinterpret(), openBtn?.reinterpret())
-    gtk_container_add(saveItem?.reinterpret(), saveBtn?.reinterpret())
-    toolItems.forEachIndexed { pos, ti ->
-        gtk_toolbar_insert(toolbar.reinterpret(), ti?.reinterpret(), pos)
-    }
-    return toolbar.reinterpret()
-}
-
 fun main() {
-    val app = gtk_application_new("org.example.kpad", G_APPLICATION_FLAGS_NONE)
-    if (app != null) connectGtkSignal(obj = app, actionName = "activate", action = staticCFunction(::activate))
-    val status = g_application_run(app?.reinterpret(), 0, null)
+    val app = gtk_application_new("org.example.kpad", G_APPLICATION_FLAGS_NONE)!!
+    connectGtkSignal(obj = app, actionName = "activate", action = staticCFunction(::activateMainWindow))
+    val status = g_application_run(app.reinterpret(), 0, null)
     g_object_unref(app)
     g_print("Application Status: %d", status)
 }
@@ -144,4 +89,25 @@ internal fun <F : CFunction<*>> connectGtkSignal(
         destroy_data = null,
         connect_flags = connectFlags
     )
+}
+
+internal fun readTextFile(filePath: String): String {
+    val size = fileSize(filePath)
+    var result = ""
+    val file = fopen(filePath, "r")
+
+    memScoped {
+        val buffer = allocArray<ByteVar>(size)
+        // Read the entire file and store the contents into the buffer.
+        fread(buffer, size.toULong(), 1, file)
+        result = buffer.toKString()
+    }
+    fclose(file)
+    return result
+}
+
+internal fun fileSize(filePath: String): Long {
+    val file = fopen(filePath, "r")
+    fseek(file, 0, SEEK_END)
+    return ftell(file)
 }
---
+++ src/linuxMain/kotlin/org/example/kpad/main_window.kt
@@ -0,0 +1,99 @@
+@file:Suppress("EXPERIMENTAL_UNSIGNED_LITERALS")
+
+package org.example.kpad
+
+import gtk3.*
+import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.staticCFunction
+
+private var editor: CPointer<GtkWidget>? = null
+private var statusBar: CPointer<GtkWidget>? = null
+private var win: CPointer<GtkWidget>? = null
+
+private fun openBtnClicked(@Suppress("UNUSED_PARAMETER") widget: CPointer<GtkWidget>, userData: gpointer) {
+    createOpenFileDialog(userData.reinterpret())
+}
+
+internal fun updateMainWindowTitle(title: String) {
+    gtk_window_set_title(win?.reinterpret(), title)
+}
+
+internal fun activateMainWindow(
+    app: CPointer<GtkApplication>,
+    @Suppress("UNUSED_PARAMETER") userData: gpointer
+) {
+    win = gtk_application_window_new(app)
+    val mainLayout = createMainLayout()
+    updateMainWindowTitle("KPad")
+    gtk_window_set_default_size(window = win?.reinterpret(), width = 600, height = 400)
+    gtk_container_add(win?.reinterpret(), mainLayout.reinterpret())
+    gtk_widget_show_all(win)
+}
+
+internal fun updateStatusBar(txt: String) {
+    gtk_statusbar_push(statusbar = statusBar?.reinterpret(), text = txt, context_id = 0)
+}
+
+internal fun updateEditor(filePath: String) {
+    val buffer = gtk_text_view_get_buffer(editor?.reinterpret())
+    gtk_text_buffer_set_text(buffer = buffer, text = readTextFile(filePath), len = fileSize(filePath).toInt())
+    gtk_widget_grab_focus(editor)
+}
+
+private fun createMainLayout(): CPointer<GtkContainer> {
+    val mainLayout = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)!!
+    val toolbar = createToolbar()
+    val scrolledWin = createScrolledWindow()
+    createStatusBar()
+
+    gtk_container_add(mainLayout.reinterpret(), toolbar.reinterpret())
+    addWidgetToBoxFromTop(box = mainLayout.reinterpret(), widget = scrolledWin.reinterpret(), expand = true)
+    addWidgetToBoxFromBottom(box = mainLayout.reinterpret(), widget = statusBar!!.reinterpret())
+    gtk_widget_grab_focus(editor?.reinterpret())
+    return mainLayout.reinterpret()
+}
+
+private fun createScrolledWindow(): CPointer<GtkScrolledWindow> {
+    val scrolledWin = gtk_scrolled_window_new(null, null)!!
+    val margins = mapOf("left" to 10, "right" to 10, "top" to 10)
+    editor = gtk_text_view_new()
+    gtk_widget_set_margin_left(scrolledWin, margins["left"]!!)
+    gtk_widget_set_margin_right(scrolledWin, margins["right"]!!)
+    gtk_widget_set_margin_top(scrolledWin, margins["top"]!!)
+    gtk_text_view_set_cursor_visible(editor?.reinterpret(), TRUE)
+    gtk_container_add(scrolledWin.reinterpret(), editor)
+    return scrolledWin.reinterpret()
+}
+
+private fun createStatusBar() {
+    statusBar = gtk_statusbar_new()
+    gtk_statusbar_push(statusbar = statusBar?.reinterpret(), text = "Ready", context_id = 0)
+}
+
+private fun createToolbar(): CPointer<GtkToolbar> {
+    val toolbar = gtk_toolbar_new()!!
+    val newBtn = gtk_button_new_with_label("New")
+    val openBtn = gtk_button_new_with_label("Open")
+    val saveBtn = gtk_button_new_with_label("Save")
+    val newItem = gtk_tool_item_new()
+    val openItem = gtk_tool_item_new()
+    val saveItem = gtk_tool_item_new()
+    val toolItems = arrayOf(newItem, openItem, saveItem)
+
+    if (openBtn != null) {
+        connectGtkSignal(
+            obj = openBtn,
+            actionName = "clicked",
+            data = win,
+            action = staticCFunction(::openBtnClicked)
+        )
+    }
+    gtk_container_add(newItem?.reinterpret(), newBtn?.reinterpret())
+    gtk_container_add(openItem?.reinterpret(), openBtn?.reinterpret())
+    gtk_container_add(saveItem?.reinterpret(), saveBtn?.reinterpret())
+    toolItems.forEachIndexed { pos, ti ->
+        gtk_toolbar_insert(toolbar.reinterpret(), ti?.reinterpret(), pos)
+    }
+    return toolbar.reinterpret()
+}
