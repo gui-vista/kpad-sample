7455988eff18345d7566abf3a5b570f3bbb32735
Nick Apperley <napperley@gmail.com>
2018-11-17 17:05:13 +1300

    Added Open File dialog.

--- src/linuxMain/kotlin/org/example/kpad/main.kt
+++ src/linuxMain/kotlin/org/example/kpad/main.kt
@@ -1,52 +1,102 @@
-@file:Suppress("EXPERIMENTAL_UNSIGNED_LITERALS")
+@file:Suppress("EXPERIMENTAL_UNSIGNED_LITERALS", "EXPERIMENTAL_API_USAGE")
 
 package org.example.kpad
 
 import gtk3.*
-import kotlinx.cinterop.CFunction
-import kotlinx.cinterop.CPointer
-import kotlinx.cinterop.reinterpret
-import kotlinx.cinterop.staticCFunction
+import kotlinx.cinterop.*
 
-private var editor: CPointer<GtkWidget>? = null
+private fun createOpenFileDialog(parent: CPointer<GtkWindow>) {
+    val gtkStockCancel = "gtk-cancel"
+    val gtkStockOpen = "gtk-open"
+    val dialogArgs = arrayOf(GTK_RESPONSE_CANCEL, gtkStockOpen, GTK_RESPONSE_ACCEPT, null)
+    val dialog = gtk_file_chooser_dialog_new(
+        parent = parent,
+        title = "Open File",
+        first_button_text = gtkStockCancel,
+        action = GtkFileChooserAction.GTK_FILE_CHOOSER_ACTION_OPEN,
+        variadicArguments = *dialogArgs
+    )
+    val resp = gtk_dialog_run(dialog?.reinterpret())
+    g_print("Response: %d\n", resp)
+    if (resp == GTK_RESPONSE_ACCEPT) {
+        // TODO: Open file and display its contents in the editor.
+        g_print("File: %s\n\n", gtk_file_chooser_get_filename(dialog?.reinterpret())?.toKString())
+    }
+    gtk_widget_destroy(dialog)
+}
+
+private fun openBtnClicked(@Suppress("UNUSED_PARAMETER") widget: CPointer<GtkWidget>, userData: gpointer) {
+    createOpenFileDialog(userData.reinterpret())
+}
 
 private fun activate(app: CPointer<GtkApplication>, @Suppress("UNUSED_PARAMETER") userData: gpointer) {
-    val win = gtk_application_window_new(app)
-    val mainLayout = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)
-    gtk_window_set_title(win?.reinterpret(), "KPad")
-    gtk_window_set_default_size(window = win?.reinterpret(), width = 600, height = 400)
-    gtk_container_add(win?.reinterpret(), mainLayout)
-    if (mainLayout != null) createToolbar(mainLayout.reinterpret())
-    if (mainLayout != null) createEditor(mainLayout.reinterpret())
-    if (mainLayout != null) createStatusBar(mainLayout.reinterpret())
-    gtk_widget_grab_focus(editor)
+    val win = gtk_application_window_new(app)!!
+    val mainLayout = createMainLayout(win.reinterpret())
+    gtk_window_set_title(win.reinterpret(), "KPad")
+    gtk_window_set_default_size(window = win.reinterpret(), width = 600, height = 400)
+    gtk_container_add(win.reinterpret(), mainLayout.reinterpret())
     gtk_widget_show_all(win)
 }
 
-private fun createEditor(container: CPointer<GtkContainer>) {
+private fun createMainLayout(win: CPointer<GtkWindow>): CPointer<GtkContainer> {
+    val mainLayout = gtk_box_new(GtkOrientation.GTK_ORIENTATION_VERTICAL, 0)!!
+    val toolbar = createToolbar(win)
+    val editor = createEditor()
+    val statusBar = createStatusBar()
+
+    gtk_container_add(mainLayout.reinterpret(), toolbar.reinterpret())
+    addWidgetToBoxFromTop(box = mainLayout.reinterpret(), widget = editor.reinterpret(), expand = true)
+    addWidgetToBoxFromBottom(box = mainLayout.reinterpret(), widget = statusBar.reinterpret())
+    gtk_widget_grab_focus(editor.reinterpret())
+    return mainLayout.reinterpret()
+}
+
+internal fun addWidgetToBoxFromTop(
+    box: CPointer<GtkBox>,
+    widget: CPointer<GtkWidget>,
+    fill: Boolean = true,
+    expand: Boolean = false,
+    padding: UInt = 0u
+): Unit = gtk_box_pack_start(
+    box = box,
+    child = widget,
+    expand = if (expand) TRUE else FALSE,
+    fill = if (fill) TRUE else FALSE,
+    padding = padding
+)
+
+internal fun addWidgetToBoxFromBottom(
+    box: CPointer<GtkBox>,
+    widget: CPointer<GtkWidget>,
+    fill: Boolean = true,
+    expand: Boolean = false,
+    padding: UInt = 0u
+): Unit = gtk_box_pack_end(
+    box = box,
+    child = widget,
+    expand = if (expand) TRUE else FALSE,
+    fill = if (fill) TRUE else FALSE,
+    padding = padding
+)
+
+private fun createEditor(): CPointer<GtkTextView> {
     val margins = mapOf("left" to 10, "right" to 10, "top" to 10)
-    editor = gtk_text_view_new()
+    val editor = gtk_text_view_new()!!
     gtk_widget_set_margin_left(editor, margins["left"]!!)
     gtk_widget_set_margin_right(editor, margins["right"]!!)
     gtk_widget_set_margin_top(editor, margins["top"]!!)
-    gtk_text_view_set_cursor_visible(editor?.reinterpret(), TRUE)
-    gtk_box_pack_start(box = container.reinterpret(), child = editor, fill = TRUE, expand = TRUE, padding = 0)
+    gtk_text_view_set_cursor_visible(editor.reinterpret(), TRUE)
+    return editor.reinterpret()
 }
 
-private fun createStatusBar(container: CPointer<GtkContainer>) {
-    val statusBar = gtk_statusbar_new()
-    gtk_statusbar_push(statusbar = statusBar?.reinterpret(), text = "Ready", context_id = 0)
-    gtk_box_pack_end(
-        box = container.reinterpret(),
-        child = statusBar?.reinterpret(),
-        fill = TRUE,
-        expand = FALSE,
-        padding = 0
-    )
+private fun createStatusBar(): CPointer<GtkStatusbar> {
+    val statusBar = gtk_statusbar_new()!!
+    gtk_statusbar_push(statusbar = statusBar.reinterpret(), text = "Ready", context_id = 0)
+    return statusBar.reinterpret()
 }
 
-private fun createToolbar(container: CPointer<GtkContainer>) {
-    val toolbar = gtk_toolbar_new()
+private fun createToolbar(win: CPointer<GtkWindow>): CPointer<GtkToolbar> {
+    val toolbar = gtk_toolbar_new()!!
     val newBtn = gtk_button_new_with_label("New")
     val openBtn = gtk_button_new_with_label("Open")
     val saveBtn = gtk_button_new_with_label("Save")
@@ -55,13 +105,16 @@ private fun createToolbar(container: CPointer<GtkContainer>) {
     val saveItem = gtk_tool_item_new()
     val toolItems = arrayOf(newItem, openItem, saveItem)
 
+    if (openBtn != null) {
+        connectGtkSignal(obj = openBtn, actionName = "clicked", data = win, action = staticCFunction(::openBtnClicked))
+    }
     gtk_container_add(newItem?.reinterpret(), newBtn?.reinterpret())
     gtk_container_add(openItem?.reinterpret(), openBtn?.reinterpret())
     gtk_container_add(saveItem?.reinterpret(), saveBtn?.reinterpret())
     toolItems.forEachIndexed { pos, ti ->
-        gtk_toolbar_insert(toolbar?.reinterpret(), ti?.reinterpret(), pos)
+        gtk_toolbar_insert(toolbar.reinterpret(), ti?.reinterpret(), pos)
     }
-    gtk_container_add(container, toolbar)
+    return toolbar.reinterpret()
 }
 
 fun main() {
